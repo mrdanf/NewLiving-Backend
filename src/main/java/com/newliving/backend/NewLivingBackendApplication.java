450ccbc815dde42715ee1fa974acbb0e50b4c292
mrdanf <dancoding@gmx.net>
2022-01-16 20:11:45 +0100

    added project

---
+++ src/main/java/com/newliving/backend/NewLivingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.newliving.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewLivingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewLivingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newliving/backend/NewLivingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newliving.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewLivingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
