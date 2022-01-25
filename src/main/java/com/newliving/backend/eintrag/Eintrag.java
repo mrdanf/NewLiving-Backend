4954a00f24960a90dca684e7aff4c8c58ed66ce5
mrdanf <dancoding@gmx.net>
2022-01-25 16:58:02 +0100

    implemented Eintrag Entity

--- src/main/java/com/newliving/backend/eintrag/Eintrag.java
+++ src/main/java/com/newliving/backend/eintrag/Eintrag.java
@@ -1,4 +1,47 @@
 package com.newliving.backend.eintrag;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
 public class Eintrag {
+
+    @Id
+    @SequenceGenerator(name = "eintrag_sequence", sequenceName = "eintrag_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "eintrag_sequence")
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private Boolean erledigt;
+
+    @Column(nullable = false)
+    private Boolean vorgabe;
+
+    private String datum;
+
+    public Eintrag(String text) {
+        this(text, false, null);
+    }
+
+    public Eintrag(String text, String datum) {
+        this(text, false, datum);
+    }
+
+    public Eintrag(String text, Boolean vorgabe, String datum) {
+        this.text = text;
+        this.erledigt = false;
+        this.vorgabe = vorgabe;
+        this.datum = datum;
+    }
 }
