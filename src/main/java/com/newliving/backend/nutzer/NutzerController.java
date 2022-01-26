4a004412582053141365f0530e1ef7b81b57444b
mrdanf <dancoding@gmx.net>
2022-01-26 23:52:30 +0100

    added JavaDoc

--- src/main/java/com/newliving/backend/nutzer/NutzerController.java
+++ src/main/java/com/newliving/backend/nutzer/NutzerController.java
@@ -5,6 +5,9 @@ import com.newliving.backend.nutzer.request.UpdatePasswortRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Schnittstellen für das Holen der Nutzerdaten, Update von Passwort und Update von Daten.
+ */
 @RestController
 @RequestMapping("/api/account")
 @AllArgsConstructor
@@ -12,17 +15,37 @@ public class NutzerController {
 
     private final NutzerService nutzerService;
 
+    /**
+     * Schnittstelle zum Holen der Nutzerdaten.
+     *
+     * @param cookieId Cookie vom eingeloggten Nutzer
+     * @return eingeloggter Nutzer, sonst exception
+     */
     @GetMapping("")
     public Nutzer getNutzer(@CookieValue(name = "JSESSIONID") String cookieId) {
         return nutzerService.getNutzerByCookie(cookieId);
     }
 
+    /**
+     * Schnittstelle zum Aktualisieren von dem Passwort.
+     *
+     * @param cookieId Cookie vom eingeloggten Nutzer
+     * @param request Request: altPasswort, neuPasswort
+     * @return
+     */
     @PutMapping("/update-passwort")
     public boolean updatePasswort(@CookieValue(name = "JSESSIONID") String cookieId,
                                   @RequestBody UpdatePasswortRequest request) {
         return nutzerService.updatePasswort(cookieId, request);
     }
 
+    /**
+     * Schnittstelle zum Aktualisieren von Nutzerdaten.
+     *
+     * @param cookieId Cookie vom eingeloggten Nutzer
+     * @param request Request: name, email, altPLZ, altAdresse, neuPLZ, neuAdresse, iban
+     * @return
+     */
     @PutMapping("/update-daten")
     public boolean updateDaten(@CookieValue(name = "JSESSIONID") String cookieId,
                               @RequestBody UpdateDatenRequest request) {
