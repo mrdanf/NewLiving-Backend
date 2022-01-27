8c2a00cb38e0e982f7cc2f13fd33da75da6ee647
mrdanf <dancoding@gmx.net>
2022-01-27 19:50:23 +0100

    added JavaDoc

--- src/main/java/com/newliving/backend/security/WebSecurityConfig.java
+++ src/main/java/com/newliving/backend/security/WebSecurityConfig.java
@@ -4,10 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * Deaktiviert die Standard Login Page von Spring Boot, damit die eigene Frontpage genutzt wird.
+     *
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
