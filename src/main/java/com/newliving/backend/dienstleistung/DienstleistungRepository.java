f61e0ccb0cec0e888077a3c73395d17265561855
mrdanf <dancoding@gmx.net>
2022-01-30 21:42:46 +0100

    dummy methods for testing

--- src/main/java/com/newliving/backend/dienstleistung/DienstleistungRepository.java
+++ src/main/java/com/newliving/backend/dienstleistung/DienstleistungRepository.java
@@ -1,9 +1,10 @@
 package com.newliving.backend.dienstleistung;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface DienstleistungRepository {
+public interface DienstleistungRepository extends JpaRepository<Dienstleistung, Long> {
 }
